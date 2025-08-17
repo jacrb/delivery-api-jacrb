33fe834eb9b3adccd4dd6e4746bad18940a4f1d7
Jacqueline Rodrigues Barbosa <bjaqueline0@gmail.com>
2025-08-17 17:10:19 -0300

    :sparkles: Adiciona controladores e serviços para gerenciamento de clientes, pedidos, produtos e restaurantes; implementa tratamento de exceções e configurações do Swagger OpenAPI

--- src/main/java/com/deliverytech/delivery/controller/ClienteController.java
+++ src/main/java/com/deliverytech/delivery/controller/ClienteController.java
@@ -2,10 +2,8 @@ package com.deliverytech.delivery.controller;
 
 
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
