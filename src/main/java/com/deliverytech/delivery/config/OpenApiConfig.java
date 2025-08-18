939e2a895e267c5d8744eeb41b93e9648473bab5
Jacqueline Rodrigues Barbosa <bjaqueline0@gmail.com>
2025-08-18 14:51:51 -0300

    :sparkles: Adiciona configuração do OpenAPI para documentação da API de Delivery

---
+++ src/main/java/com/deliverytech/delivery/config/OpenApiConfig.java
@@ -0,0 +1,32 @@
+package com.deliverytech.delivery.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+  @Bean
+  public OpenAPI customOpenAPI() {
+    return new OpenAPI()
+        .info(new Info()
+            .title("Delivery API")
+            .version("1.0")
+            .description("API para sistema de delivery")
+            .contact(new Contact()
+                .name("Delivery Tech")
+                .email("contato@deliverytech.com")))
+        .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+        .components(new io.swagger.v3.oas.models.Components()
+            .addSecuritySchemes("bearerAuth", new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")));
+  }
+}
+
